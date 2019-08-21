29854ca2f6098147edf5d535910f32a0ac092a48
Wyesumu <wyesumu@gmail.com>
2019-08-21 10:06:07 +0700

    added stages

--- main.py
+++ main.py
@@ -18,7 +18,10 @@ from PIL import ImageFont, ImageFilter, ImageDraw
 from resize_and_crop import resize_and_crop
 from random import randrange
 #https connection
-#from OpenSSL import SSL
+from OpenSSL import SSL
+#flask_admin
+from flask_admin import Admin, AdminIndexView, expose
+from flask_admin.contrib.sqla import ModelView
 
 app = flask.Flask(__name__)
 
@@ -26,25 +29,38 @@ bcrypt = Bcrypt(app)
 app.secret_key = config.secret_key
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + config.db_file
+#app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
-#<databases classes>
+#------------------------<databases classes>------------------------------
+
+class emptyUser():
+
+	id = 0
+	username = None
+	password = None
+	privileges = 3
+	last_post = None
 
 class User(db.Model):
 
-	id = db.Column(db.Integer, primary_key=True)
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 	username = db.Column(db.String())
+	Email = db.Column(db.String())
+	FullName = db.Column(db.String())
 	password = db.Column(db.String())
-	privileges = db.Column(db.Integer, default = 3)
+	privileges = db.Column(db.Integer, db.ForeignKey("privilege.id"), nullable=False, default = 3)
 	images = db.relationship('Image', backref='users', lazy=True)
 	last_post = db.Column(db.DateTime)
+	winner_of = db.relationship('Stage', backref='user_won', lazy=True)
+	profile_pic = db.Column(db.String(), default="default_pic.jpg")
 
 	def __repr__(self):
 		return self.username
 
 class Image(db.Model):
 
-	id = db.Column(db.Integer, primary_key=True)
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 	addr = db.Column(db.String())
 	user = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
 	username = db.relationship("User", back_populates="images")
@@ -52,15 +68,40 @@ class Image(db.Model):
 	note = db.Column(db.String())
 	date = db.Column(db.DateTime, nullable=False)
 	extras = db.relationship('Extra', backref='images', lazy=True)
+	stage = db.Column(db.Integer, db.ForeignKey("stage.id"), nullable=False)
 
 class Extra(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
+
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 	image = db.Column(db.Integer, db.ForeignKey("image.id"), nullable=False)
 	addr = db.Column(db.String())
 
-#/database classes/
+class Stage(db.Model):
 
-#<tool functions>
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	start_date = db.Column(db.DateTime, nullable=False)
+	end_date = db.Column(db.DateTime)
+	user_win = db.Column(db.Integer, db.ForeignKey("user.id"))
+	user = db.relationship("User", backref="stage_user")
+	images = db.relationship('Image', backref='stages', lazy=True)
+
+	def __repr__(self):
+		return "Этап № {}".format(self.id)
+
+class Privilege(db.Model):
+
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	name = db.Column(db.String())
+	users = db.relationship("User", backref="tags")
+
+	def __repr__(self):
+		return self.name
+
+db.create_all()
+
+#------------------/database classes/---------------------------
+
+#-------------------<tool functions>---------------------------
 
 #decorator to check if user logged in
 def is_logged(func):
@@ -81,10 +122,13 @@ def allowed_file(files): #check if file extension is allowed
 			allowed = False
 	return allowed
 
-def getLastDate():
+def getLastDate(user_id = None, stage_id = Stage.query.all()[-1].id):
 	#this function finds date of the most recent post
 	#get dates of all images by username
-	images = Image.query.filter_by(user=flask.session['user_id']).all()
+	if not user_id:
+		user_id = flask.session['user_id']
+
+	images = Image.query.filter(Image.user == user_id, Image.stage == stage_id).all()
 	datetime_list = []
 	try:
 		for image in images:
@@ -106,15 +150,89 @@ def time_left(): #calculate how many time left until block
 	except Exception as e:
 		print(e)
 
-def getUsername():
-	if 'username' in flask.session:
-		return flask.session['username']
+def getUser():
+	if 'logged' in flask.session:
+		return User.query.get(flask.session['user_id'])
 	else:
-		return None
+		return emptyUser()
 
-#/tool functions/
+#is user allowed to access this page
+def user_allowed(level):
+	if getUser().privileges <= level:
+		return True
+	else:
+		return False
 
-#<paths for static files>
+#------------------/tool functions/---------------------------
+
+#------------------<flask_admin>--------------------------------
+
+#restict access to /admin index
+class MyAdminIndexView(AdminIndexView):
+
+	def is_visible(self):
+		return False
+
+	def is_accessible(self):
+		return user_allowed(1)
+
+#list of users in admin
+class UserView(ModelView):
+
+	column_exclude_list = ['password','stage_user']
+	form_excluded_columns = ['password','stage_user']
+
+	#restict access to user list
+	def is_accessible(self):
+		return user_allowed(1)
+
+class ImageView(ModelView):
+
+	column_exclude_list = ['username',]
+	form_excluded_columns = ['username',]
+
+	column_display_pk = True
+
+	#column_sortable_list = ('id')
+
+	column_labels = {'addr':"Имя файла", 
+					"name":"Название", 
+					"note":"Примечание", 
+					"date":"Дата",
+					"extras":"Доп. изображения",
+					"users":"Автор",
+					"stages":"Этап"}
+
+	def is_accessible(self):
+		return user_allowed(1)
+
+class StageView(ModelView):
+
+	column_display_pk = True
+	column_exclude_list = ['username','user']
+	form_excluded_columns = ['username','user']
+
+	#column_sortable_list = ('id')
+
+	column_labels = {'id':"Номер", 
+					"start_date":"Время начала", 
+					"end_date":"Время завершения", 
+					"images":"Изображения",
+					"user_won":"Победитель"
+					}
+
+	def is_accessible(self):
+		return user_allowed(1)
+
+#initialize admin views
+admin = Admin(app, name='1day1page', template_mode='bootstrap3', index_view=MyAdminIndexView(), url='/')
+admin.add_view(UserView(User, db.session, 'Пользователи', url='/admin/user'))
+admin.add_view(ImageView(Image, db.session, 'Изображения', url='/admin/image'))
+admin.add_view(StageView(Stage, db.session, 'Этапы', url='/admin/stage'))
+
+#---------------------/flask_admin/---------------------
+
+#-----------------<paths for static files>-------------------
 
 @app.route('/fonts/<path:path>')
 def send_fonts(path):
@@ -132,9 +250,9 @@ def send_images(path):
 def send_thumbain(path):
 	return flask.send_from_directory('thumbnails', path)
 
-#/paths for static files/
+#------------------/paths for static files/------------------
 
-#<session control>
+#--------------------<session control>----------------------
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
@@ -147,10 +265,10 @@ def login():
 			if bcrypt.check_password_hash(user_data.password, str(password)): #compare user input and password hash from db
 				#set session info in crypted session cookie
 				flask.session['logged'] = "yes"
-				flask.session['username'] = login
+				#flask.session['username'] = login
 				flask.session['user_id'] = user_data.id
-				flask.session['privileges'] = user_data.privileges
-				flask.session['last_post'] = user_data.last_post.strftime("%Y-%m-%d %H:%M")
+				#flask.session['privileges'] = user_data.privileges
+				flask.session['last_post'] = getLastDate().strftime("%Y-%m-%d %H:%M")
 				return flask.redirect(flask.url_for("index"))
 			else:
 				flask.flash("Wrong password. Try again")
@@ -165,14 +283,18 @@ def register():
 	
 		if [''] in f.values(): #if there's any empty field
 			flask.flash("Не все поля заполнены")
+			return flask.redirect(flask.url_for("register"))
 		elif f['password'][0] != f['password_check'][0]: #if passwords doesn't match
 			flask.flash("Пароли не совпадают")
+			return flask.redirect(flask.url_for("register"))
 		elif f['pass_phrase'][0] != config.pass_phrase: #if passphrase is wrong
 			flask.flash('Неверно введена кодовая фраза')
+			return flask.redirect(flask.url_for("register"))
 		else:
 			#check if username already in db
 			if User.query.filter_by(username = f["login"][0]).first():
 				flask.flash("Пользователь с таким именем уже существует")
+				return flask.redirect(flask.url_for("register"))
 			else:
 				#create new user
 				new_user = User(username = f["login"][0], last_post = dt.now(), password = bcrypt.generate_password_hash(str(f["password"][0])))
@@ -183,26 +305,48 @@ def register():
 	else:
 		return flask.render_template("register.html")
 
-@app.route("/exit", methods=["POST"])
+@app.route("/exit", methods=["GET"])
 def exit():
-	exit = flask.request.form['exit']
-	if exit:
-		flask.session.clear() #clear session when user log out
-		return flask.redirect(flask.url_for("login"))
+	flask.session.clear() #clear session when user log out
+	return flask.redirect(flask.url_for("login"))
 
-#/session control/
+#---------------------/session control/--------------------------
 
 @app.route("/")
 def index():
 	#get posts from db for gallery
-	data = Image.query.order_by(Image.id.desc())
-	return flask.render_template("index.html", data=data, username=getUsername(), time_left = time_left())
+	stage_id = flask.request.args.get("stage")
+	stages = Stage.query.all()
+	if stage_id:
+		data = Image.query.filter_by(stage = stage_id).order_by(Image.id.desc())
+		if stages[int(stage_id)-1].user_win:
+			User.survived = getLastDate(user_id = stages[int(stage_id)-1].user_win, stage_id=stage_id) - stages[int(stage_id)-1].start_date
+	else:
+		data = Image.query.filter_by(stage = stages[-1].id).order_by(Image.id.desc())
+		if stages[-1].user_win:
+			User.survived = getLastDate(user_id = stages[-1].user_win) - stages[-1].start_date
+	return flask.render_template("index.html", stages=stages, data=data, user=getUser(), time_left=time_left())
 
-@app.route('/user/<username>') #one artist only gallery
+
+@app.route('/user/<username>', methods=["GET","POST"]) #one artist only gallery
 def user_gallery(username):
-	data = Image.query.order_by(Image.id.desc()).filter_by(user = User.query.filter_by(username=username).first().id)
-	#using index template here, just return different data
-	return flask.render_template("index.html", data=data, username=getUsername())
+	if flask.request.method == 'GET':
+		#data = Image.query.order_by(Image.id.desc()).filter(Image.stage == Stage.query.all()[-1].id, Image.user == User.query.filter_by(username=username).first().id)
+		data = User.query.filter_by(username=username).first()
+		User.survived = getLastDate(user_id = data.id) - Stage.query.all()[-1].start_date
+		return flask.render_template("profile.html", data=data, user=getUser())
+	else:
+		files = flask.request.files.getlist("file")
+		user = getUser()
+		if files[0] and allowed_file(files):
+			image = resize_and_crop(files[0], (256,256), "middle")
+			filename = "profile_" + str(randrange(10000,100000)) + '.' + files[0].filename.rsplit('.', 1)[1].lower()
+			image.save(os.path.join(config.THUMBNAIL_FOLDER, filename))
+			user.profile_pic = filename
+			db.session.commit()
+			flask.flash("Загружено")
+		return flask.redirect(flask.url_for("stats"))
+
 
 @app.route("/upload", methods=["GET","POST"])
 @is_logged
@@ -210,16 +354,28 @@ def upload():
 #
 # This function is just a mess. I'm so sorry :-:
 #
-	data = User.query.get(flask.session['user_id']) #get last post date
-	#dt.strptime(str(data.last_post), "%Y-%m-%d %H:%M")
-	delta = dt.now() - data.last_post # and calculate how many time left since this date
+	data = getUser() #get last post date
 
-	if (delta.days * 24) + (delta.seconds // 3600) > 80 or data.privileges == 4: # if amout of hours less than 80 and level of privileges < 4 (new user get 3. 4 means admin blocked access for user manually)
+	delta = dt.now() - data.last_post # and calculate how many time left since this date
+	delta_last_post = (delta.days * 24) + (delta.seconds // 3600)
+	delta = dt.now() - Stage.query.all()[-1].start_date
+	delta_stage_start = (delta.days * 24) + (delta.seconds // 3600)
+
+	if delta_last_post < 80 or delta_stage_start < 80:
+		not_allowed = False
+	else:
+		not_allowed = True
+
+	if data.privileges <= 2:
+		not_allowed = False
+
+	# if amout of hours less than 80 and level of privileges < 4 (new user get 3. 4 means admin blocked access for user manually)
+	if not_allowed or data.privileges == 4: 
 		flask.flash("Вы не создавали записи более 3 дней. Доступ закрыт")
 		return flask.redirect(flask.url_for("index"))
 	else:
 		if flask.request.method == 'GET':
-			return flask.render_template("upload.html", username=flask.session['username'])
+			return flask.render_template("upload.html", user=data)
 		else:
 			# check if the post request has the file part
 			if 'file' not in flask.request.files:
@@ -294,14 +450,14 @@ def upload():
 						time_input = flask.request.form['time_input']
 						LastPostDate = flask.request.form['time_input']
 
-				new_image = Image(addr=filename[0], user=flask.session['user_id'], name=name, note=flask.request.form['note'],date=dt.strptime(str(time_input), "%Y-%m-%d %H:%M"))
+				new_image = Image(addr=filename[0], user=flask.session['user_id'], name=name, note=flask.request.form['note'],date=dt.strptime(str(time_input), "%Y-%m-%d %H:%M"), stage=Stage.query.all()[-1].id)
 				db.session.add(new_image)
 				db.session.flush()
-				print(files)
-				print(filename)
 				for i in range(1,len(files)):
 					new_extra = Extra(image=new_image.id, addr=filename[i])
 					db.session.add(new_extra)
+				update_user = User.query.get(flask.session['user_id'])
+				update_user.last_post = dt.strptime(str(LastPostDate), "%Y-%m-%d %H:%M")
 				db.session.commit()
 				flask.session['last_post'] = LastPostDate #and also update cookies
 
@@ -310,8 +466,6 @@ def upload():
 				flask.flash("Invalid file type")
 				return flask.redirect(flask.url_for("index"))
 			
-	
-		
 
 @app.route('/refresh_time') #refresh time in times on index
 def get_left_time():
@@ -320,14 +474,14 @@ def get_left_time():
 	return flask.redirect(flask.url_for("index"))
 
 @app.route('/stats', methods=["GET", "POST"])
-@is_logged
 def stats():
 	if flask.request.method == "POST":
 		r = flask.request.form.to_dict() #get all fields in dict
 		if "reset" in r: #if admin pushed reset button to give access to user back
 			user = User.query.get(r["reset"])
 			user.last_post = dt.now()
-			user.privileges = 3
+			if user.privileges == 4:
+				user.privileges = 3
 			flask.flash("Доступ пользователю " + user.username + " восстановлен")
 			db.session.commit()
 			return flask.redirect(flask.url_for("stats"))
@@ -338,8 +492,8 @@ def stats():
 			db.session.commit()
 			return flask.redirect(flask.url_for("stats"))
 	else: #GET request
-		users = User.query.filter(User.privileges > 1).all()
-		return flask.render_template("stats.html", username=flask.session['username'], users = users, privileges=flask.session['privileges'])
+		users = User.query.all()
+		return flask.render_template("stats.html", user=getUser(), users = users)
 
 @app.route('/image/<int:image_id>', methods=["GET","POST"]) #this is what inside the post
 def full_image(image_id):
@@ -375,16 +529,12 @@ def full_image(image_id):
 	#if we just want to show bigger image
 	else: #if GET request return images
 		image = Image.query.get(image_id)
-		if 'username' in flask.session:
-			privileges = flask.session['privileges']
-		else:
-			privileges = 3
-		return flask.render_template("image.html", username=getUsername(), image=image, privileges=privileges)
+		return flask.render_template("image.html", user=getUser(), image=image)
 
 
 @app.route('/cal') #page with calendar
 def calendar():
-	return flask.render_template("cal.html", username = getUsername())
+	return flask.render_template("cal.html", user = getUser())
 
 @app.route('/data') #send JSON events to calendar
 def return_data():
@@ -399,4 +549,4 @@ def return_data():
 
 #run only if started standalone, not imported
 if __name__ == "__main__":
-	app.run(host=config.host, port=config.port) #, ssl_context='adhoc'
+	app.run(host=config.host, port=config.port, ssl_context='adhoc')
